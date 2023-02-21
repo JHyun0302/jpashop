67019e75269f2c2fb64f571ddfbe090e044f602a
jaehyun <wogustkfkd11@gmail.com>
2023-02-21 22:18:48 +0900

    배달 정보(1) - order 정보(1, 주인)

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,27 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+@Setter
+public class Delivery {
+    @Id
+    @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery", fetch = LAZY) //거울 역할(읽기 전용)
+    private Order order;
+
+    @Embedded //내장 타입
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status; //ENUM [READY(준비), COMP(배송)]
+}
