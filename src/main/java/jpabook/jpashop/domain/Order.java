c498a66c6619fad63cf3f48c9eb0a7387fd263c9
jaehyun <wogustkfkd11@gmail.com>
2023-02-23 12:21:21 +0900

    주문 생성, 취소, 전체 주문 가격 조회 "로직"

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -14,6 +16,7 @@ import static javax.persistence.FetchType.LAZY;
 @Table(name = "orders") //Table명 "orders"로 변경
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) //다른 곳에서 new OrderItem(); 생성 막아줌
 public class Order {
     @Id
     @GeneratedValue
@@ -62,4 +65,44 @@ public class Order {
         delivery.setOrder(this);
     }
 
+    //==생성 메서드==// 앞으로 생성하는 지점 변경 시에는 여기만 수정하면 됨!
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) { //"OrderItem...": 여러 개 매개변수 받기 가능
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //==비지니스 로직==//
+
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    //==조회 로직==//
+
+    /**
+     * 전체 주문 가격 조회(람다 가능)
+     */
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
 }
