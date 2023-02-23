c72309f33744590dd44a4c9dc3de33e222f005c5
jaehyun <wogustkfkd11@gmail.com>
2023-02-23 12:22:32 +0900

    ItemService는 ItemRepository에 단순히 위임만 하는 클래스 (굳이 필요있을까?)

---
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * ItemService는 ItemRepository에 단순히 위임만 하는 클래스 (굳이 필요있을까?)
+ * Controller -> ItemRepository 로 바로 이동하는 것도 고려해보기!
+ */
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+}
