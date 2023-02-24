75b94cb752a3bedda51c05d82a247a1cd19bcf88
jaehyun <wogustkfkd11@gmail.com>
2023-02-24 21:50:06 +0900

    변경 감시 예시

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -87,7 +87,7 @@ public class Order {
         if (delivery.getStatus() == DeliveryStatus.COMP) {
             throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
         }
-        this.setStatus(OrderStatus.CANCEL);
+        this.setStatus(OrderStatus.CANCEL); //값만 바꾸고 update query 안 썼다! -> JPA: 변경감지
         for (OrderItem orderItem : orderItems) {
             orderItem.cancel();
         }
