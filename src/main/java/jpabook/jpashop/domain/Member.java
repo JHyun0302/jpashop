5cef824867247f74d2ff827d544478fe3206fa66
jaehyun <wogustkfkd11@gmail.com>
2023-02-25 18:16:28 +0900

    @JsonIgnore: API 통신 시 원하는 데이터만 받기

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,9 +1,11 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,11 +18,13 @@ public class Member {
     @Column(name = "member_id") //PK 컬럼명
     private Long id;
 
+    @NotEmpty
     private String name;
 
     @Embedded
     private Address address;
 
+    @JsonIgnore //순수하게 회원 정보만 api로 받을 때 order 정보 없애기
     @OneToMany(mappedBy = "member") //Order 필드의 member로 mapping 되어짐!(거울 - 읽기전용; 값을 넣어도 FK값 변경 안됨!)
     private List<Order> orders = new ArrayList<>();
 }
