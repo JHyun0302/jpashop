1b5a71970594ae38aed53ead513686e8fc748757
jaehyun <wogustkfkd11@gmail.com>
2023-02-26 19:21:45 +0900

    스프링 데이터 JPA

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -43,13 +43,13 @@ public class MemberService {
     }
 
     public Member findOne(Long memberId) {
-        return memberRepository.findOne(memberId);
+        return memberRepository.findById(memberId).get();
     }
 
     //회원 수정: 변경 감지
     @Transactional
     public void update(Long id, String name) {
-        Member member = memberRepository.findOne(id);
+        Member member = memberRepository.findById(id).get();
         member.setName(name);
     }
 }
